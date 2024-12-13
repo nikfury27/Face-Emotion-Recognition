ce9cc6a0aac16ce7f92c20ec857d97f49e43b140
Nikhil M <157052646+nikfury27@users.noreply.github.com>
2024-12-13 12:39:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from keras.models import load_model
 
-model=load_model('model_file_improved.h5')
+model=load_model('model_file_improvedd.h5')
 
 video=cv2.VideoCapture(0)
 
